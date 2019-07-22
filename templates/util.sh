a92dda4bd673950945ac9cbbc782faf5dcda020d
Sainnhepark <sainnhe@gmail.com>
2019-07-23 03:11:19 +0800

    use $ROOT_DIR in util.sh

--- templates/util.sh
+++ templates/util.sh
@@ -1,15 +1,17 @@
 #!/bin/env bash
 
-mv -f ../colors/gruvbox_material.vim ../colors/gruvbox-material.vim
-mv -f ../colors/gruvbox_material_hard.vim ../colors/gruvbox-material-hard.vim
-mv -f ../colors/gruvbox_material_soft.vim ../colors/gruvbox-material-soft.vim
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+mv -f $ROOT_DIR/colors/gruvbox_material.vim $ROOT_DIR/colors/gruvbox-material.vim
+mv -f $ROOT_DIR/colors/gruvbox_material_hard.vim $ROOT_DIR/colors/gruvbox-material-hard.vim
+mv -f $ROOT_DIR/colors/gruvbox_material_soft.vim $ROOT_DIR/colors/gruvbox-material-soft.vim
 
 sed -i\
     -re "s/let g:colors_name = 'gruvbox_material'/let g:colors_name = 'gruvbox-material'/"\
-    ../colors/gruvbox-material.vim
+    $ROOT_DIR/colors/gruvbox-material.vim
 sed -i\
     -re "s/let g:colors_name = 'gruvbox_material_hard'/let g:colors_name = 'gruvbox-material-hard'/"\
-    ../colors/gruvbox-material-hard.vim
+    $ROOT_DIR/colors/gruvbox-material-hard.vim
 sed -i\
     -re "s/let g:colors_name = 'gruvbox_material_soft'/let g:colors_name = 'gruvbox-material-soft'/"\
-    ../colors/gruvbox-material-soft.vim
+    $ROOT_DIR/colors/gruvbox-material-soft.vim
