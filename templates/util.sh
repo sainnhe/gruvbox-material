dcf69bfbf3f84d4e965aa61269536f50ecf38532
Sainnhepark <sainnhe@gmail.com>
2019-12-25 20:58:28 +0800

    refactor(templates): remove color templates

--- templates/util.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-ROOT_DIR=$(git rev-parse --show-toplevel)
-
-mv -f $ROOT_DIR/colors/gruvbox_material.vim $ROOT_DIR/colors/gruvbox-material.vim
-
-sed -ri\
-    -e "s/let g:colors_name = 'gruvbox_material'/let g:colors_name = 'gruvbox-material'/"\
-    $ROOT_DIR/colors/gruvbox-material.vim
