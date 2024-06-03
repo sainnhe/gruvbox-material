sainnhe/gruvbox-material

commit 8ca1c0c6d7b8282a01c3404a50c89cb0c0be6fb3

fix(lsp): use distinct styles for (non-)underline diags
Ref. sainnhe/everforest#137

colors/gruvbox-material.vim
@@ -10,7 +10,7 @@
 let s:configuration = gruvbox_material#get_configuration()
 let s:palette = gruvbox_material#get_palette(s:configuration.background, s:configuration.foreground, s:configuration.colors_override)
 let s:path = expand('<sfile>:p') " the path of this script
-let s:last_modified = 'Sat Jun  1 03:11:50 AM UTC 2024'
+let s:last_modified = 'Sun Jun  2 08:41:16 UTC 2024'
 let g:gruvbox_material_loaded_file_types = []
 
 if !(exists('g:colors_name') && g:colors_name ==# 'gruvbox-material' && s:configuration.better_performance)
@@ -279,22 +279,33 @@ call gruvbox_material#highlight('debugBreakpoint', s:palette.bg0, s:palette.red)
 call gruvbox_material#highlight('ToolbarButton', s:palette.bg0, s:palette.grey2)
 if has('nvim')
   call gruvbox_material#highlight('Substitute', s:palette.bg0, s:palette.yellow)
+  if s:configuration.diagnostic_text_highlight
+    call gruvbox_material#highlight('DiagnosticError', s:palette.red, s:palette.bg_red)
+    call gruvbox_material#highlight('DiagnosticUnderlineError', s:palette.none, s:palette.bg_red, 'undercurl', s:palette.red)
+    call gruvbox_material#highlight('DiagnosticWarn', s:palette.yellow, s:palette.bg_yellow)
+    call gruvbox_material#highlight('DiagnosticUnderlineWarn', s:palette.none, s:palette.bg_yellow, 'undercurl', s:palette.yellow)
+    call gruvbox_material#highlight('DiagnosticInfo', s:palette.blue, s:palette.bg_blue)
+    call gruvbox_material#highlight('DiagnosticUnderlineInfo', s:palette.none, s:palette.bg_blue, 'undercurl', s:palette.blue)
+    call gruvbox_material#highlight('DiagnosticHint', s:palette.green, s:palette.bg_green)
+    call gruvbox_material#highlight('DiagnosticUnderlineHint', s:palette.none, s:palette.bg_green, 'undercurl', s:palette.green)
+  else
+    call gruvbox_material#highlight('DiagnosticError', s:palette.red, s:palette.none)
+    call gruvbox_material#highlight('DiagnosticUnderlineError', s:palette.none, s:palette.none, 'undercurl', s:palette.red)
+    call gruvbox_material#highlight('DiagnosticWarn', s:palette.yellow, s:palette.none)
+    call gruvbox_material#highlight('DiagnosticUnderlineWarn', s:palette.none, s:palette.none, 'undercurl', s:palette.yellow)
+    call gruvbox_material#highlight('DiagnosticInfo', s:palette.blue, s:palette.none)
+    call gruvbox_material#highlight('DiagnosticUnderlineInfo', s:palette.none, s:palette.none, 'undercurl', s:palette.blue)
+    call gruvbox_material#highlight('DiagnosticHint', s:palette.green, s:palette.none)
+    call gruvbox_material#highlight('DiagnosticUnderlineHint', s:palette.none, s:palette.none, 'undercurl', s:palette.green)
+  endif
   highlight! link DiagnosticFloatingError ErrorFloat
   highlight! link DiagnosticFloatingWarn WarningFloat
   highlight! link DiagnosticFloatingInfo InfoFloat
   highlight! link DiagnosticFloatingHint HintFloat
-  highlight! link DiagnosticError ErrorText
-  highlight! link DiagnosticWarn WarningText
-  highlight! link DiagnosticInfo InfoText
-  highlight! link DiagnosticHint HintText
   highlight! link DiagnosticVirtualTextError VirtualTextError
   highlight! link DiagnosticVirtualTextWarn VirtualTextWarning
   highlight! link DiagnosticVirtualTextInfo VirtualTextInfo
   highlight! link DiagnosticVirtualTextHint VirtualTextHint
-  highlight! link DiagnosticUnderlineError ErrorText
-  highlight! link DiagnosticUnderlineWarn WarningText
-  highlight! link DiagnosticUnderlineInfo InfoText
-  highlight! link DiagnosticUnderlineHint HintText
   highlight! link DiagnosticSignError RedSign
   highlight! link DiagnosticSignWarn YellowSign
   highlight! link DiagnosticSignInfo BlueSign

commit 905a41c86dbfed42f89c13c36f0e3407e9318910

fix: reference to unknown palette color 'bg_blue'
The issue was introduced in 8ca1c0c6d7b8282a01c3404a50c89cb0c0be6fb3

Fixes #207

colors/gruvbox-material.vim
@@ -280,14 +280,14 @@ call gruvbox_material#highlight('ToolbarButton', s:palette.bg0, s:palette.grey2)
 if has('nvim')
   call gruvbox_material#highlight('Substitute', s:palette.bg0, s:palette.yellow)
   if s:configuration.diagnostic_text_highlight
-    call gruvbox_material#highlight('DiagnosticError', s:palette.red, s:palette.bg_red)
-    call gruvbox_material#highlight('DiagnosticUnderlineError', s:palette.none, s:palette.bg_red, 'undercurl', s:palette.red)
-    call gruvbox_material#highlight('DiagnosticWarn', s:palette.yellow, s:palette.bg_yellow)
-    call gruvbox_material#highlight('DiagnosticUnderlineWarn', s:palette.none, s:palette.bg_yellow, 'undercurl', s:palette.yellow)
-    call gruvbox_material#highlight('DiagnosticInfo', s:palette.blue, s:palette.bg_blue)
-    call gruvbox_material#highlight('DiagnosticUnderlineInfo', s:palette.none, s:palette.bg_blue, 'undercurl', s:palette.blue)
-    call gruvbox_material#highlight('DiagnosticHint', s:palette.green, s:palette.bg_green)
-    call gruvbox_material#highlight('DiagnosticUnderlineHint', s:palette.none, s:palette.bg_green, 'undercurl', s:palette.green)
+    call gruvbox_material#highlight('DiagnosticError', s:palette.red, s:palette.bg_visual_red)
+    call gruvbox_material#highlight('DiagnosticUnderlineError', s:palette.none, s:palette.bg_visual_red, 'undercurl', s:palette.red)
+    call gruvbox_material#highlight('DiagnosticWarn', s:palette.yellow, s:palette.bg_visual_yellow)
+    call gruvbox_material#highlight('DiagnosticUnderlineWarn', s:palette.none, s:palette.bg_visual_yellow, 'undercurl', s:palette.yellow)
+    call gruvbox_material#highlight('DiagnosticInfo', s:palette.blue, s:palette.bg_visual_blue)
+    call gruvbox_material#highlight('DiagnosticUnderlineInfo', s:palette.none, s:palette.bg_visual_blue, 'undercurl', s:palette.blue)
+    call gruvbox_material#highlight('DiagnosticHint', s:palette.green, s:palette.bg_visual_green)
+    call gruvbox_material#highlight('DiagnosticUnderlineHint', s:palette.none, s:palette.bg_visual_green, 'undercurl', s:palette.green)
   else
     call gruvbox_material#highlight('DiagnosticError', s:palette.red, s:palette.none)
     call gruvbox_material#highlight('DiagnosticUnderlineError', s:palette.none, s:palette.none, 'undercurl', s:palette.red)